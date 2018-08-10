50f6ff6ae0dd34470cad73ef5a7531a8d1cf670b
Austin Cummings <Austins.accounts@gmail.com>
2018-08-10 13:00:12 -0700

    begin logical work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+var ruby;
+var php;
+var java;
+
+var selectionPath = function(selection) {
+  alert("ruby precount");
+  if(selection === "track1")
+  {
+    ruby = ruby + 1;
+    alert("this fired")
+  }
+  if(selection === "track2")
+  {
+    alert("you blew2");
+  }
+  return $(ruby+1);
+};
+
+// var i;
+// for (i = 0; i < 9; i++) {
+// var j = j++;
+// $(selectionPath(choice"" + j)));
+// }
+
+
 //business logic
 
 
@@ -6,27 +31,26 @@
 $(document).ready(function() {
   $("#trackSelection").submit(function(event) {
     event.preventDefault();
+    ruby = 0;
+    java = 0;
+    php = 0;
 
-    var choice1 = $('input[name=q1]:checked', '#trackSelection').val()
-    var choice2 = $('input[name=q2]:checked', '#trackSelection').val()
-    var choice3 = $('input[name=q3]:checked', '#trackSelection').val()
-    var choice4 = $('input[name=q4]:checked', '#trackSelection').val()
-    var choice5 = $('input[name=q5]:checked', '#trackSelection').val()
-    var choice6 = $('input[name=q6]:checked', '#trackSelection').val()
-    var choice7 = $('input[name=q7]:checked', '#trackSelection').val()
-    var choice8 = $('input[name=q8]:checked', '#trackSelection').val()
-    var choice9 = $('input[name=q9]:checked', '#trackSelection').val()
+    var choice1 = $('input[name=q1]:checked', '#trackSelection').val();
+    var choice2 = $('input[name=q2]:checked', '#trackSelection').val();
+    var choice3 = $('input[name=q3]:checked', '#trackSelection').val();
+    var choice4 = $('input[name=q4]:checked', '#trackSelection').val();
+    var choice5 = $('input[name=q5]:checked', '#trackSelection').val();
+    var choice6 = $('input[name=q6]:checked', '#trackSelection').val();
+    var choice7 = $('input[name=q7]:checked', '#trackSelection').val();
+    var choice8 = $('input[name=q8]:checked', '#trackSelection').val();
+    var choice9 = $('input[name=q9]:checked', '#trackSelection').val();
 
-    var tester = $(parseInt(3));
-
-
-    console.log(parseInt(tester)+parseInt(tester));
- $()
-    // var triquery= function(side1, side2, side3) {
-
-    //   console.log(side1, side2, side3);
-    //   return("");
-    // };
-      $("#results").append(choice1);
+    var i;
+for (i = 0; i < 9; i++) {
+    var j = j++;
+    $(selectionPath(choice"" + j));
+}
+    //   // $("#results").append(choice1);
+    console.log(choice + j);
     });
   });
