8a6499df00682b28cb5cc9c073bffad89a7067c1
Austin Cummings <Austins.accounts@gmail.com>
2018-08-10 17:54:57 -0700

    redo html presentation

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,16 @@ console.log("selection path fired")
 var whatPath = function(java, ruby, php) {
   if (java > ruby && java > php)
   {
-    return "trackJava";
+    return("trackJava");
   }
   if (php > ruby && php > java)
   {
-    return "trackPhp";
+    return("trackPhp");
   }
   if (ruby > java &&  ruby > php)
   {
-    return "trackRuby";
+
+    return("trackRuby");
   }
 };
 //business logic
@@ -40,18 +41,15 @@ var whatPath = function(java, ruby, php) {
 $(document).ready(function() {
   $("#trackSelection").submit(function(event) {
     event.preventDefault();
-    ruby = 0;
     java = 0;
     php = 0;
+    ruby = 0;
+
     var choice1 = $('input[name=q1]:checked', '#trackSelection').val();
     var choice2 = $('input[name=q2]:checked', '#trackSelection').val();
     var choice3 = $('input[name=q3]:checked', '#trackSelection').val();
     var choice4 = $('input[name=q4]:checked', '#trackSelection').val();
     var choice5 = $('input[name=q5]:checked', '#trackSelection').val();
-    var choice6 = $('input[name=q6]:checked', '#trackSelection').val();
-    var choice7 = $('input[name=q7]:checked', '#trackSelection').val();
-    var choice8 = $('input[name=q8]:checked', '#trackSelection').val();
-    var choice9 = $('input[name=q9]:checked', '#trackSelection').val();
     var j = 0;
     var i;
 
@@ -60,11 +58,10 @@ $(document).ready(function() {
     $(selectionPath(choice3));
     $(selectionPath(choice4));
     $(selectionPath(choice5));
-    $(selectionPath(choice6));
-    $(selectionPath(choice7));
-    $(selectionPath(choice8));
-    $(selectionPath(choice9));
-    //this could be done easily with a loop??//
-
+    var thisthing = whatPath(java, php, ruby);
+  $("#results").append(whatPath(java, php, ruby));
+$("#trackSelection").hide();
+$("#"+thisThing).show();
+  console.log(thisthing);
     });
   });
