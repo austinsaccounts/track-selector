57afb4a390a9ba063ba9dbb439f6811868032fb9
Austin Cummings <Austins.accounts@gmail.com>
2018-08-17 17:04:01 -0700

    renames questions

--- js/scripts.js
+++ js/scripts.js
@@ -43,11 +43,11 @@ $(document).ready(function() {
     php = 0;
     ruby = 0;
 
-    var choice1 = $('input[name=q1]:checked', '#trackSelection').val();
-    var choice2 = $('input[name=q2]:checked', '#trackSelection').val();
+    var choice1 = $('input[name=intrigue]:checked', '#trackSelection').val();
+    var choice2 = $('input[name=wants]:checked', '#trackSelection').val();
     var choice3 = $('input[name=q3]:checked', '#trackSelection').val();
-    var choice4 = $('input[name=q4]:checked', '#trackSelection').val();
-    var choice5 = $('input[name=q5]:checked', '#trackSelection').val();
+    var choice4 = $('input[name=feelings]:checked', '#trackSelection').val();
+    var choice5 = $('input[name=betterquestions]:checked', '#trackSelection').val();
     var userName = $('#txt_name').val();
 
 
